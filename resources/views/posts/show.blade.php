7b3d6af0e96bec36cd8362936d685e01e1476a07
QAPsyker <philbertpereira@gmail.com>
2017-05-29 18:22:41 +0300

    Comments

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -4,5 +4,20 @@
     <div class="col-sm-8 blog-main">
         <h1>{{$post->title}}</h1>
         <p>{{$post->body}}</p>
+
+        @if(count($post->comments))
+        <hr>
+        <div class="comments">
+            <h3>Comments:</h3>
+            <ul class="list-group">
+                @foreach($post->comments as $comment)
+                    <li class="list-group-item">
+                        <strong>{{$comment->created_at->diffForHumans()}}</strong> : &nbsp;
+                        {{$comment->body}}
+                    </li>
+                @endforeach
+            </ul>
+        </div>
+        @endif
     </div>
 @endsection
