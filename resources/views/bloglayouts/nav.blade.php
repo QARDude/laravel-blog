1285a585c1bf4785edfd3d687207c880d9ce7a58
QAPsyker <philbertpereira@gmail.com>
2017-05-29 20:19:36 +0300

    Auth

--- resources/views/bloglayouts/nav.blade.php
+++ resources/views/bloglayouts/nav.blade.php
@@ -3,7 +3,7 @@
         <nav class="nav blog-nav">
           <a class="nav-link active" href="/posts">Home</a>
           <a class="nav-link" href="/posts/create">Create New Post</a>
-          <a class="nav-link" href="#">Press</a>
+          <a class="nav-link" href="/register">Register</a>
           <a class="nav-link" href="#">New hires</a>
           <a class="nav-link" href="#">About</a>
         </nav>
---
+++ resources/views/sessions/create.blade.php
@@ -0,0 +1,11 @@
+@extends('bloglayouts.master')
+
+@section('pagetitle')
+    The Blog : Register
+@endsection
+
+@section('content')
+    <div class="col-sm-8 blog-main">
+        <h1>Register</h1>
+    </div>
+@endsection
