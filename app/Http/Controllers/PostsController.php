4e09a92dc4ff496d23f50971ed0c34411000fa72
ppereira <QARDude@users.noreply.github.com>
2017-05-30 13:05:50 +0300

    Archives

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Post;
+use Carbon\Carbon;
 
 class PostsController extends Controller
 {
@@ -11,8 +12,25 @@ class PostsController extends Controller
         $this->middleware('auth')->except('index','show');
     }
     public function index() {
-        $posts = Post::latest()->get();
-        return view('posts.index', compact('posts'));
+        $posts = Post::latest();
+
+        if($month = request('month')) {
+            $posts->whereMonth('created_at', Carbon::parse($month)->month);
+        }
+
+        if($year = request('year')) {
+            $posts->whereYear('created_at', $year);
+        }
+        
+        $posts = $posts->get();
+
+        $archives = Post::selectRaw('year(created_at) year, monthname(created_at) month, count(*) published')
+                                    -> groupBy('year','month')
+                                    -> orderByRaw('min(created_at)')
+                                    -> get()
+                                    -> toArray();
+                                    
+        return view('posts.index', compact('posts', 'archives'));
     }
 
     public function show(Post $post) {
--- resources/views/bloglayouts/sidebar.blade.php
+++ resources/views/bloglayouts/sidebar.blade.php
@@ -1,7 +1,19 @@
         <div class="col-sm-3 offset-sm-1 blog-sidebar">
-          @include('bloglayouts.about')
+          <div class="sidebar-module sidebar-module-inset">
+            <h4>About</h4>
+            <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
+          </div>
           <div class="sidebar-module">
-           @include('bloglayouts.archive')
+            <h4>Archives</h4>
+            <ol class="list-unstyled">
+              @foreach($archives as $stats)
+              <li>
+                  <a href="/posts/?month={{$stats['month']}}&year={{$stats['year']}}">
+                    {{$stats['month'] . ', ' . $stats['year']}}
+                  </a>
+              </li>
+              @endforeach
+            </ol>
           </div>
           <div class="sidebar-module">
             <h4>Elsewhere</h4>
